bdfbe3f5a868214b0ce1bb93f573d413a2489093
kingofwhales <wileyfan1992@gmail.com>
2016-06-22 15:43:25 -0400

    Set up files for Todo part 2

---
+++ week2/server.rb
@@ -0,0 +1,6 @@
+#Inside of server.rb
+require 'sinatra'
+require 'sinatra/reloader'
+# We're going to need to require our class files
+require_relative('lib/Task.rb')
+require_relative('lib/TodoList.rb')
