039e786d74bce8e88da9fc839cb9ad5207730b61
kingofwhales <wileyfan1992@gmail.com>
2016-06-26 17:14:20 -0400

    Stuck. Js and css file not updating.

--- week2/day5/public/home.js
+++ week2/day5/public/home.js
@@ -1,26 +1,14 @@
 function ask_question(){
 	console.log(arrays)
 	console.log("afdafew")
-
-	var x = Math.floor((Math.random() * 3));s
-
+	var x = Math.floor((Math.random() * 3));
 
 	// var array1 = [years,ratings,directors]
 
-	// for (a=0;a<array1.length;a++){
-	// 	arrays_for_this_round = array1[x]
-
-
-
-	// }
+	
 	alert ("Which one of this movie has ratings lower than 5?")
 
 
 
 
-}
-
-
-function addslashes( str ) {
-    return (str + '').replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
 }
