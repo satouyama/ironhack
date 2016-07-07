be9c91eb133f9244e0b05d38c65eb85aaa7f64c5
kingofwhales <wileyfan1992@gmail.com>
2016-07-07 14:18:00 -0400

    Added strong parameters and partials

--- week4/timetracking/app/controllers/time_entries_controller.rb
+++ week4/timetracking/app/controllers/time_entries_controller.rb
@@ -20,12 +20,7 @@ class TimeEntriesController < ApplicationController
   def create 
     @my_project = Project.find(params[:project_id])
 
-    @my_entry = @my_project.time_entries.new(
-          hours: params[:time_entry][:hours],
-          minutes: params[:time_entry][:minutes],
-          date: params[:time_entry][:date],
-          comments: params[:time_entry][:comments]
-      )
+    @my_entry = @my_project.time_entries.new(entry_params)
 
     if @my_entry.save
       redirect_to "/projects/#{@my_project.id}/time_entries"
@@ -33,6 +28,48 @@ class TimeEntriesController < ApplicationController
       render "new"
     end
 
+
+  end
+
+
+  def edit
+    @my_project = Project.find(params[:project_id])
+
+    @my_entry = @my_project.time_entries.find params[:id]
+
+    render "edit"
+
+
+  end
+
+
+  def update
+    @my_project = Project.find_by(id: params[:project_id])
+
+    @my_entry = @my_project.time_entries.find_by(id: params[:id])
+
+    if @my_entry.update(entry_params)
+      redirect_to action: "index", controller: "time_entries", project_id: @my_project.id
+    else
+      redirect_to :back
+      # render "edit"
+    end
+  end
+
+  private
+
+  def entry_params
+
+  #   {
+  #   hours: params[:time_entry][:hours],
+  #   minutes: params[:time_entry][:minutes],
+  #   date: params[:time_entry][:date],
+  #   comments: params[:time_entry][:comments]
+  #   }
+
+
+    params.require(:time_entry).permit(:hours,:minutes,:date,:comments)    #require the key time_entry if not in ,then error
+    # then insdie time_entry only allows these 4 inputs .
 
   end
 end
--- week4/timetracking/config/routes.rb
+++ week4/timetracking/config/routes.rb
@@ -7,15 +7,22 @@ Rails.application.routes.draw do
 
   get "/projects" => "projects#index"
 
-  get "projects/new" => "projects#new"  # has to go before projects/:id
+  get "/projects/new" => "projects#new"  # has to go before projects/:id
   get "/projects/:id" => "projects#show"
 
   post "/projects" => "projects#create"
 
-  get "projects/:project_id/time_entries" => "time_entries#index"
+  get "/projects/:project_id/time_entries" => "time_entries#index"
 
   get "/projects/:project_id/time_entries/new" => "time_entries#new"
 
   post "/projects/:project_id/time_entries" => "time_entries#create", as: :project_time_entries
+
+  get "/projects/:project_id/time_entries/:id/edit" => "time_entries#edit"
+
+  patch "/projects/:project_id/time_entries/:id" => "time_entries#update", as: :project_time_entry
+
+
+
 
 end
