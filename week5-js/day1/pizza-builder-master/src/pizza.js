003cc54dddc7c25794fe40f4bd7d4013e0ddda3f
kingofwhales <wileyfan1992@gmail.com>
2016-07-11 23:10:19 -0400

    change prices

--- week5-js/day1/pizza-builder-master/src/pizza.js
+++ week5-js/day1/pizza-builder-master/src/pizza.js
@@ -1,13 +1,27 @@
 // Write your Pizza Builder JavaScript in this file.$(function(){
 	// alert("haha")
  $(function(){
+
+
+
 	$(".btn-pepperonni").click(function(){
+		// console.log("change runs first")
+		// var a = $(".btn-pepperonni")
+
+		// console.log(a)
+		pep_counter = !pep_counter
+
+		if (pep_counter == true){
+			$("#pep").show()
+		}else {
+			$("#pep").hide()
+		}
 		$(".pep").fadeToggle("fast","linear",function(){  // called once per matched element
 			if (($(".pep").is(":visible"))){
-				$(".btn-pepperonni").removeClass("active")
+				$(".btn-pepperonni").addClass("active")
 				console.log("x");
 			}else{
-				$(".btn-pepperonni").addClass("active")
+				$(".btn-pepperonni").removeClass("active")
 				console.log("y")
 			}
 		})
@@ -16,6 +30,14 @@
 
 
 	$(".btn-mushrooms").click(function(){
+		mush_counter = !mush_counter
+
+		if (mush_counter == true){
+			$("#mush").show()
+		}else {
+			$("#mush").hide()
+		}
+
 		if (($(".mushroom").is(":visible"))){
 			$(".btn-mushrooms").removeClass("active")
 			console.log("x");
@@ -34,6 +56,12 @@
 
 
 	$(".btn-green-peppers").click(function(){
+		green_counter = !green_counter
+		if (green_counter == true){
+			$("#green").show()
+		}else {
+			$("#green").hide()
+		}
 		if (($(".green-pepper").is(":visible"))){
 			$(".btn-green-peppers").removeClass("active")
 			console.log("x");
@@ -44,14 +72,22 @@
 		$(".green-pepper").fadeToggle()
 	})
 
+	$(".btn-sauce").removeClass("active")
+	$("#sauce").hide()
 
 	$(".sauce-white").hide()
 	$(".btn-sauce").click(function(){
+		white_counter = !white_counter
+		if (white_counter == true){
+			$("#sauce").show()
+		}else {
+			$("#sauce").hide()
+		}
 		if (($(".sauce").is(":visible"))){
-			$(".btn-sauce").removeClass("active")
+			$(".btn-sauce").addClass("active")
 			console.log("x");
 		}else{
-			$(".btn-sauce").addClass("active")
+			$(".btn-sauce").removeClass("active")
 			console.log("y")
 		}
 
@@ -61,7 +97,15 @@
 
 	$(".crust-gluten-free").removeClass("crust-gluten-free")
 
+	$(".btn-crust").removeClass("active")
+	$("#crust").hide()
 	$(".btn-crust").click(function(){
+		glutten_counter =!glutten_counter
+		if (glutten_counter == true){
+			$("#crust").show()
+		}else {
+			$("#crust").hide()
+		}
 		if (($(".crust-gluten-free").is(":visible"))){
 			$(".btn-crust").addClass("active")
 		}else{
@@ -70,6 +114,76 @@
 		}
 
 		$(".crust").toggleClass("crust-gluten-free")
+
+	})
+	var pep_counter = true;
+	var mush_counter = true;
+	var green_counter = true;
+	var white_counter = false;
+	var glutten_counter = false;
+	var final = pep_counter * 1 + mush_counter * 1 + green_counter * 1
+
+	$("strong").text("$" + final)
+
+	$(".btn").click(function(){
+		final = pep_counter * 1 + mush_counter * 1 + green_counter * 1 + white_counter * 3 + glutten_counter * 5
+		$("strong").text("$" + final)
+
+
+		// console.log("price runs first")
+
+		// var button_list = $(".btn")
+
+		// console.log(pep_counter + " " + mush_counter + " " +green_counter + " " +white_counter + " " +glutten_counter)
+
+		// for (var counter = 0;counter<button_list.length;counter++){
+		// 	switch (button_list.eq(counter).prop("class")){
+		// 		case "btn btn-pepperonni active":
+		// 			pep_counter = !pep_counter;
+		// 			console.log(pep_counter)
+		// 			break;
+		// 		case "btn btn-pepperonni":
+		// 			pep_counter = !pep_counter;
+		// 			console.log(pep_counter)
+
+		// 			break;
+		// 		case "btn btn-mushrooms active":
+		// 			mush_counter = !mush_counter;
+
+		// 			break;
+		// 		case "btn btn-mushrooms":
+		// 			mush_counter = !mush_counter;
+
+		// 			break;
+		// 		case "btn btn-green-peppers active":
+		// 			green_counter = !green_counter;
+
+		// 			break;
+		// 		case "btn btn-green-peppers":
+		// 			green_counter = !green_counter;
+
+		// 			break;
+		// 		case "btn btn-sauce active":
+		// 			white_counter = !white_counter;
+
+		// 			break;
+		// 		case "btn btn-sauce":
+		// 			white_counter = !white_counter;
+
+		// 			break;
+		// 		case "btn btn-crust active":
+		// 			glutten_counter = !glutten_counter;
+
+		// 			break;
+		// 		case "btn btn-crust":
+		// 			glutten_counter = !glutten_counter;
+
+		// 			break;
+			
+		
+		// console.log(pep_counter + " " + mush_counter + " " +green_counter + " " +white_counter + " " +glutten_counter)
+		
+
 
 	})
 
